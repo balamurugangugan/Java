c9da446cce1fe0e6fb57cbc182abca60301dba6e
balamurugangugan <balamurugangugan.s@gmail.com>
2025-12-03 02:10:12 +0530

    Updated Patterns Code

--- Basic/Patterns.java
+++ Basic/Patterns.java
@@ -1,7 +1,209 @@
 package Java.Basic;
+import java.util.*;
+import java.io.*;
 
 public class Patterns {
     public static void main(String[] args){
-        System.out.println("Hi");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the value n: ");
+        int n = scan.nextInt();
+        Patterns pattern = new Patterns();
+        pattern.pattern13(n);
     }
+    
+    void pattern13(int n){
+        for(int i = 1; i<=n; i++){
+            //for last line
+            while(i==n){
+                for(int y = 1; y<=n; y++){
+                    System.out.print("*");
+                }
+            break; 
+            }
+            
+            // section 1
+            for(int j = n-i; j>0; j--){
+                System.out.print(" ");
+            }
+
+            System.out.print("*");
+
+            //section3 spaces
+            while(i>1 || i<n){
+                for(int j = 2; j<=i ; j++){
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            break; 
+            }
+
+            System.out.println("");
+        }
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+    void pattern12(int n){  
+     
+    System.out.println("Similar to previous ones");
+
+
+    }
+
+    void pattern11(int n){  
+     
+    System.out.println("Similar to previous ones");
+
+
+    }
+     
+    
+    void pattern10(int n){  
+     
+    System.out.println("Similar to previous ones");
+
+
+    }
+
+    void pattern9(int n){
+        for(int i = 1; i<=n; i++){
+        //section1
+            for(int j = 1; j<i; j++){
+                System.out.print("  ");
+            }
+        //section2
+             for (int a = 1; a<=n-i+1; a++) {
+                System.out.print("* ");
+            }
+        
+        //section3
+             for (int a = 1; a<=n-i; a++) {
+                System.out.print("* ");
+            } 
+         System.out.println();
+        }
+     }
+    
+    
+    void pattern8(int n){
+        for(int i = 1; i<=n; i++){
+        //section1
+            for(int j = 1; j<=n-i; j++){
+                System.out.print("  ");
+            }
+        //section2
+             for (int a = 1; a <= i; a++) {
+                System.out.print("* ");
+            }
+        
+        //section3
+        
+            for (int a = 1; a < i; a++) {
+                 System.out.print("* ");
+            }
+        
+            /*
+                for(int k = 1; k<=(1+(2*i)) ; k++){
+                    System.out.println("* ");
+            }
+        //section4
+        
+            for (int j = n-i; i > 0; j--) {
+                System.out.print("  ");
+            }*/
+
+         System.out.println();
+        }
+     }
+
+
+
+    void pattern7(int n){
+       for(int i = 1; i<=n; i++){
+         for(int j = 1; j<= i-1; j++){
+            System.out.print("  ");
+         }
+         for(int k = n-i+1; k>0; k--){
+            System.out.print("* ");
+         }  
+        System.out.println();
+       }
+    }
+
+
+
+    void pattern6(int n){
+    for (int i = 1; i <= n ; i++) {
+        for(int j = n - i; j>0; j--){
+            System.out.print("  ");
+        }
+        for (int j = 1; j <= i ; j++) {
+            System.out.print("* ");
+        }
+        System.out.println("");
+     }
+    }
+    void pattern5(int n){
+    for(int i = 1;  i<=n; i++){
+      for (int j = 1; j <= i; j++) {
+          System.out.print("* ");
+      }
+      System.out.println();
+     }
+     for(int i = n; i>1; i--){
+      for (int j = 1; j < i; j++) {
+          System.out.print("* ");
+      }
+      System.out.println();
+     }
+    }
+
+    
+    void pattern4(int n){
+    for(int i = 1;  i<=n; i++){
+      for (int j = 1; j <= i; j++) {
+          System.out.print(j);
+      }
+      System.out.println();
+     }
+    }
+    
+    void pattern3(int n){
+       for(int i=1; i<=n; i++){// Creates n number of line
+       // code block to be performed from 1 to n number line
+        for(int j=n-i+1; j>0; j--){// now for each line say i = 1, so here the loop performs for 1x1, 1x2 ,......,1xn
+        System.out.print("* ");
+        }
+        System.out.println();// need to move to second
+      }
+    }  
+
+    void pattern2(int n){
+       for(int i=1; i<=n; i++){// Creates n number of line
+       // code block to be performed from 1 to n number line
+        for(int j=1; j<=i; j++){// now for each line say i = 1, so here the loop performs for 1x1, 1x2 ,......,1xn
+        System.out.print(j);
+        }
+        System.out.println();// need to move to second
+      }
+    }  
+  
+    void pattern1(int n){
+       for(int i=1; i<=n; i++){// Creates n number of line
+       // code block to be performed from 1 to n number line
+        for(int j=1; j<=n; j++){// now for each line say i = 1, so here the loop performs for 1x1, 1x2 ,......,1xn
+        System.out.print("* ");
+        }
+        System.out.println();// need to move to second
+    }
+ }
 }
